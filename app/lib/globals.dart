7359a94b1d7c366a4fc9d038c2d0505a7431a3af
wim <van.laer.wim@gmail.com>
2022-01-05 18:20:37 +0100

    basic app setup

---
+++ app/lib/globals.dart
@@ -0,0 +1,10 @@
+// ignore: depend_on_referenced_packages
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_gen/gen_l10n/app_localizations_nl.dart';
+
+typedef S = AppLocalizations;
+typedef SNl = AppLocalizationsNl;
+typedef SEn = AppLocalizationsEn;
