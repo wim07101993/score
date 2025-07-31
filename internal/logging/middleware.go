4cf5929e683a0d4668696cbe4973b9d05ce42de9
Wim Van Laer <van.laer.wim@gmail.com>
2025-07-31 16:48:08 +0200

    feat: accept correlation id header

--- internal/logging/middleware.go
+++ internal/logging/middleware.go
@@ -10,15 +10,18 @@ import (
 
 func Wrap(l *slog.Logger, handler func(res http.ResponseWriter, req *http.Request) error) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
-		correlation := uuid.New().String()
+		correlationId := req.Header.Get("X-Correlation-ID")
+		if correlationId == "" {
+			correlationId = uuid.New().String()
+		}
 		l.Info("handle http request",
 			slog.String("method", req.Method),
 			slog.String("pattern", req.Pattern),
 			slog.String("uri", req.RequestURI),
-			slog.String("correlationId", correlation))
+			slog.String("correlationId", correlationId))
 
-		req = req.WithContext(context.WithValue(req.Context(), internal.CorrelationIdKey, correlation))
-		loggingRes := NewResponseWriter(l, res, correlation)
+		req = req.WithContext(context.WithValue(req.Context(), internal.CorrelationIdKey, correlationId))
+		loggingRes := NewResponseWriter(l, res, correlationId)
 		defer loggingRes.Flush()
 
 		loggingRes.Err = handler(loggingRes, req)
